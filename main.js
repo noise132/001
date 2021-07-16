dda5c1cc8818115656ceac3303a78b24a9a79963
noise132 <37348775+noise132@users.noreply.github.com>
2021-07-17 02:08:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,17 @@
 import {
   DragResizeBlock,
 } from 'react-native-drag-resize-element';
+
+
+<DragResizeBlock
+  x={0}
+  y={0}
+>
+  <View
+    style={{
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'red',
+    }}
+  />
+</DragResizeBlock>
